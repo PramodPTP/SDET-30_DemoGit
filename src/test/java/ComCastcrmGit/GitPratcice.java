c0c9de1a23c17b955dcb54a4e74b92d3b36eeb37
Pramod <Pramod@PRAMODTP7795.mshome.net>
2022-03-10 10:14:23 +0530

    user 1--second commit

---
+++ src/test/java/ComCastcrmGit/GitPratcice.java
@@ -0,0 +1,12 @@
+package ComCastcrmGit;
+
+import org.testng.annotations.Test;
+
+public class GitPratcice {
+
+	
+	@Test
+	public void Githubpractice() {
+		System.out.println("user 2 -- first commit");
+	}
+}
